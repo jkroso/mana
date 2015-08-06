9493c130d962e73a7e2cddf37bd7475ef203957b
Jake Rosoman <jkroso@gmail.com>
2015-08-07 09:47:41 +1200

    tabs to spaces

--- app.js
+++ app.js
@@ -13,35 +13,35 @@ const {NODE} = require('./')
  */
 
 class App {
-	constructor(state, render, location) {
-		this.isRendering = false
-		this.redrawScheduled = false
-		this.atom = state instanceof Atom ? state : new Atom(state)
-		this.state = new Cursor(this.atom)
-		this.UI = render(this.state)
+  constructor(state, render, location) {
+    this.isRendering = false
+    this.redrawScheduled = false
+    this.atom = state instanceof Atom ? state : new Atom(state)
+    this.state = new Cursor(this.atom)
+    this.UI = render(this.state)
 
-		this.redraw = () => {
-			this.redrawScheduled = false
-			this.isRendering = true
-			this.UI = this.UI.update(render(this.state))
-			this.isRendering = false
-			this.onRedraw()
-		}
+    this.redraw = () => {
+      this.redrawScheduled = false
+      this.isRendering = true
+      this.UI = this.UI.update(render(this.state))
+      this.isRendering = false
+      this.onRedraw()
+    }
 
-		this.atom.addListener(() => {
-			this.state = new Cursor(this.atom)
-			assert(!this.isRendering, 'redraw requested while rendering')
-			if (this.redrawScheduled) return
-			this.redrawScheduled = true
-			requestAnimationFrame(this.redraw)
-		})
+    this.atom.addListener(() => {
+      this.state = new Cursor(this.atom)
+      assert(!this.isRendering, 'redraw requested while rendering')
+      if (this.redrawScheduled) return
+      this.redrawScheduled = true
+      requestAnimationFrame(this.redraw)
+    })
 
-		// wait for document.body to be defined
-		domready(() => this.UI.mount(location || document.body))
-	}
+    // wait for document.body to be defined
+    domready(() => this.UI.mount(location || document.body))
+  }
 
-	// Overwrite if you want to save the state or something
-	onRedraw(){}
+  // Overwrite if you want to save the state or something
+  onRedraw(){}
 }
 
 /**
@@ -49,52 +49,52 @@ class App {
  */
 
 function stopPropagation() {
-	this.cancelBubble = true
+  this.cancelBubble = true
 }
 
 const dispatchEvent = e => {
-	e.stopPropagation = stopPropagation
-	var {target,type} = e
-	while (target) {
-		var node = target[NODE]
-		target = target.parentNode
-		if (node == null) continue
-		var fn = node.events[type]
-		fn && fn.call(node, e)
-		if (e.cancelBubble) break
-	}
+  e.stopPropagation = stopPropagation
+  var {target,type} = e
+  while (target) {
+    var node = target[NODE]
+    target = target.parentNode
+    if (node == null) continue
+    var fn = node.events[type]
+    fn && fn.call(node, e)
+    if (e.cancelBubble) break
+  }
 }
 
 ;[
-	'click',
-	'mousedown',
-	'mouseup',
-	'dblclick',
-	'mousedown',
-	'mouseup',
-	'mouseover',
-	'mousemove',
-	'mouseout',
-	'dragstart',
-	'drag',
-	'dragenter',
-	'dragleave',
-	'dragover',
-	'drop',
-	'dragend',
-	'keydown',
-	'keypress',
-	'keyup',
-	'resize',
-	'scroll',
-	'select',
-	'change',
-	'submit',
-	'reset',
-	'focus',
-	'blur',
-	'focusin',
-	'focusout'
+  'click',
+  'mousedown',
+  'mouseup',
+  'dblclick',
+  'mousedown',
+  'mouseup',
+  'mouseover',
+  'mousemove',
+  'mouseout',
+  'dragstart',
+  'drag',
+  'dragenter',
+  'dragleave',
+  'dragover',
+  'drop',
+  'dragend',
+  'keydown',
+  'keypress',
+  'keyup',
+  'resize',
+  'scroll',
+  'select',
+  'change',
+  'submit',
+  'reset',
+  'focus',
+  'blur',
+  'focusin',
+  'focusout'
 ].forEach(event => window.addEventListener(event, dispatchEvent, true))
 
 export default App
